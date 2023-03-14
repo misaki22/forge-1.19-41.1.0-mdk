154b4e35a281b3cd9373635385514c3a3fb541cd
Mitsuo Sánchez <mitsuo22jun@gmail.com>
2023-03-13 21:45:08 -0500

    Custom items and item tab on the creative menu

---
+++ src/main/java/net/misaki/tutorialmod/item/ModCreativeModTab.java
@@ -0,0 +1,14 @@
+package net.misaki.tutorialmod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModTab {
+    public static final CreativeModeTab TUTORIAL_TAB = new CreativeModeTab("tutorialtab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.ZIRCON.get());
+        }
+    };
+
+}
